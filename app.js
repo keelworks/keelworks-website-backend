8740402f5d4c996f5a5d3310dc51830c52309ec7
Shabahang Saffari <shabahang.saffari@gmail.com>
2024-10-21 01:08:04 -0700

    initial setup of the server

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Hello From KeelWorks");
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5500;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, msg: "Welcome to KeelWorks back-end application" });
+});
+
+
+
+
+
+
+
+// *****************************
+// ********** Server ***********
+// *****************************
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
