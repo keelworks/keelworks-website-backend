ad51d9f7129ca6a2476012c54caa45eb4e6b7dfa
Shabahang Saffari <shabahang.saffari@gmail.com>
2024-11-06 23:50:06 -0800

    removing multer from SignUp route

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const multer = require('multer');
-const upload = multer();
+// const multer = require('multer');
+// const upload = multer();
 
 const{signup} = require('../controllers/signupOperations');
 
-router.route('/').post(upload.none(), signup);
+router.route('/').post(signup);
 
 module.exports = router;
