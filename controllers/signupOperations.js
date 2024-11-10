ff9ba93ef3aea458f0b7e0149f861073142fb33b
Shabahang Saffari <shabahang.saffari@gmail.com>
2024-11-09 21:12:20 -0800

    Signup operations receiving data from front-end

--- controllers/signupOperations.js
+++ controllers/signupOperations.js
@@ -3,7 +3,7 @@ const FormData = require('form-data');
 const {StatusCodes} = require('http-status-codes');
 const {BadRequestError} = require('../errors');
 
-const urlTest = 'https://script.google.com/macros/s/AKfycbytKVKRPQA03UYxoyWhOUprekY10vKSPdDpFGpyheqsQ21uvG1SBCg7MsDETatOsdKf/exec';
+// const urlTest = 'https://script.google.com/macros/s/AKfycbytKVKRPQA03UYxoyWhOUprekY10vKSPdDpFGpyheqsQ21uvG1SBCg7MsDETatOsdKf/exec';
 // const realKeelWorksURL = 'https://script.google.com/macros/s/AKfycbwwlgVzJv42ERjRKso-pk8Xu9HD3GZl4nxdEltQ3nEzNcX_f9U0_U2ww4kZPlqZi51_Yg/exec';
 
 
@@ -35,12 +35,64 @@ const signup = async(req, res)=>{
       data.append("Working Days", filteredData.daysCheckbox);
       data.append("Working Hours", filteredData.workingHours);
 
+      // /*----------------Append References in Request Payload------------*/
+
+      data.append(
+        "Reference 1 First Name",
+        filteredData.reference1RefFirstName ?? " "
+      );
+      data.append(
+        "Reference 1 Last Name",
+        filteredData.reference1RefLastName ?? " "
+      );
+      data.append("Reference 1 Email", filteredData.reference1RefEmail ?? " ");
+      data.append(
+        "Reference 1 Contact No",
+        filteredData.reference1RefContactNo ?? " "
+      );
+      data.append("Reference 1 City", filteredData.reference1RefCity ?? " ");
+      data.append("Reference 1 State", filteredData.reference1RefState ?? " ");
+      data.append(
+        "Reference 1 Country",
+        filteredData.reference1RefCountry ?? " "
+      );
+      data.append(
+        "Reference 1 Relationship",
+        filteredData.reference1RelationshipToRef ?? " "
+      );
+
+      data.append(
+        "Reference 2 First Name",
+        filteredData.reference2RefFirstName ?? " "
+      );
+      data.append(
+        "Reference 2 Last Name",
+        filteredData.reference2RefLastName ?? " "
+      );
+      data.append("Reference 2 Email", filteredData.reference2RefEmail ?? " ");
+      data.append(
+        "Reference 2 Contact No",
+        filteredData.reference2RefContactNo ?? " "
+      );
+      data.append("Reference 2 City", filteredData.reference2RefCity ?? " ");
+      data.append("Reference 2 State", filteredData.reference2RefState ?? " ");
+      data.append(
+        "Reference 2 Country",
+        filteredData.reference2RefCountry ?? " "
+      );
+      data.append(
+        "Reference 2 Relationship",
+        filteredData.reference2RelationshipToRef ?? " "
+      );
+
+      /*----------------Append References in Request Payload------------*/
+
     //   const response = await fetch(url, {
     //   method: 'POST',
     //   body: formData,
     // });
 
-      console.log("Your email has been sent successfully!", data);
+      console.log("Your email has been sent successfully!", filteredData);
       // console.log("Here is FormData:", formData);
     }
   } 
